e0f77cd4d72d2f6728f599c88965d43e7f5ff56f
nano <tchal004@umn.edu>
2019-12-15 19:19:29 -0800

    pushing iOS project

---
+++ stem_club/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stem_club
+//
+//  Created by Kevin Tchalla on 12/13/19.
+//  Copyright © 2019 NIA STEM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
